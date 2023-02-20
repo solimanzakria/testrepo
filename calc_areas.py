40626144d0808482ffb79cfe4ec8a0d6a4afdd10
solimanzakria <solimanzakria@gmail.com>
2023-02-20 13:40:28 +0200

    Add files via upload

---
+++ calc_areas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 12:06:45 2022
+
+@author: pc
+"""
+
+import areae
+print(areae.triangle(3,5))
+print(areae.rectangle(3,5))
+print(areae.circle(5)) 
